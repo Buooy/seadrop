eb41ce3df886ab7a4bf976693f3cf23be010348a
Buooy <aaron.lee@buooy.com>
2023-08-01 18:26:53 +0800

    test(erc721a): added withdrawal tests

--- src-upgradeable/test/nft/withdraw.test.ts
+++ src-upgradeable/test/nft/withdraw.test.ts
@@ -4,19 +4,60 @@ import { ethers } from "hardhat";
 import { baseContract } from "./__fixtures__";
 
 describe("Token (Withdraw)", function () {
-  it("only allows contract owner", async () => {
-    const { nft, dev } = await loadFixture(baseContract);
-    await expect(nft.connect(dev).withdraw()).to.be.reverted;
+  let nft;
+  let owner;
+  let ownerAddress;
+  let externalAccount;
+  let externalAccountAddress;
+  const mintPrice = ethers.utils.parseEther("0.1");
+
+  before(async () => {
+    const {
+      nft: _nft,
+      owner: _owner,
+      ownerAddress: _ownerAddress,
+      externalAccountAddress: _externalAccountAddress,
+      externalAccount: _externalAccount,
+    } = await loadFixture(baseContract);
+
+    nft = _nft;
+    owner = _owner;
+    ownerAddress = _ownerAddress;
+    externalAccountAddress = _externalAccountAddress;
+    externalAccount = _externalAccount;
+
+    //  Mint tokens
+    await nft.connect(owner).setDirectMint(true);
+    await nft.connect(owner).setMaxSupply(100);
   });
+
+  it("only allows contract owner to set finance wallet", async () => {
+    await expect(
+      nft.connect(externalAccount).setFinanceWallet(ownerAddress)
+    ).to.be.revertedWith("OnlyOwner()");
+
+    await nft.connect(owner).setFinanceWallet(ownerAddress);
+    expect(await nft.financeWallet()).to.equal(ownerAddress);
+  });
+
+  it("only allows contract owner to withdraw", async () => {
+    await expect(nft.connect(externalAccount).withdraw()).to.be.revertedWith(
+      "OnlyOwner()"
+    );
+  });
+
   it("withdraws", async () => {
-    const { nft, owner, externalAccount } = await loadFixture(baseContract);
-    const MINTER_ROLE = await nft.MINTER_ROLE();
-    await nft.grantRole(MINTER_ROLE, externalAccount.address);
-    await nft.connect(externalAccount).mint([1], [externalAccount.address], {
-      value: ethers.utils.parseEther("10"),
-    });
     const currentBalance = await owner.getBalance();
-    await nft.withdraw();
-    expect(await owner.getBalance()).to.be.greaterThanOrEqual(currentBalance);
+
+    await nft
+      .connect(externalAccount)
+      .mint(5, externalAccountAddress, { value: mintPrice.mul(5).toString() });
+
+    await nft.connect(owner).withdraw();
+
+    const newBalance = await owner.getBalance();
+    const minNewBalance = currentBalance.add(mintPrice.mul(4));
+
+    expect(newBalance.gte(minNewBalance)).to.be.true;
   });
 });
